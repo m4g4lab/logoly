c509e6e97dc3fbc0e22e2d9badc4632cdd1f9c33
lurenyang <lurenyang@outlook.com>
2025-07-11 16:06:42 +0800

    update: 补充一个像素字体

--- src/components/FontSelector.vue
+++ src/components/FontSelector.vue
@@ -1,19 +1,17 @@
 <template>
-  <SelectRoot default-value="Roboto" class="w-full flex flex-row gap-1">
-    <SelectTrigger class="border-transparent hover:border hover:border-1 rounded-1 outline-none hover:border-#f90 h-10">
-      <SelectValue class="text-#f90 px-1" />
+  <SelectRoot v-model="store.font">
+    <SelectTrigger class="bg-black rounded-1 border-0.5 border-#f90 h-8">
+      <SelectValue class="text-#f90 px-2" />
     </SelectTrigger>
     <SelectPortal>
       <SelectContent
+        :side-offset="1"
         class="min-w-[180px] h-50 bg-black rounded-lg border shadow-sm will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade z-[100]">
-        <SelectViewport>
+        <SelectViewport class="p-5px">
           <SelectGroup>
             <SelectItem v-for="(option, index) in fonts" :key="index"
-              class="text-xs leading-none  rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-green9 data-[highlighted]:text-green1"
+              class="text-xs leading-none rounded-[3px] flex items-center h-[25px] pl-[5px] relative select-none data-[state=checked]:bg-#ff9900"
               :value="option">
-              <SelectItemIndicator class="absolute left-0 w-[25px] inline-flex items-center justify-center">
-                <svg class="i-lucide-check"></svg>
-              </SelectItemIndicator>
               <SelectItemText class="text-white">
                 {{ option }}
               </SelectItemText>
@@ -131,5 +129,6 @@ const fonts = [
   'PT Sans Caption',
   'Alegreya Sans',
   'Source Code Pro',
+  "Pixelify Sans",
 ];
 </script>
